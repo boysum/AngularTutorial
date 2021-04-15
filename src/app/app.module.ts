7f9986eb37a494622abf0c1f97cde9f435a89170
Rob McGraw <rmcgraw@lagoonpark.com>
2021-04-15 09:06:23 -0600

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
+import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule
   ],
